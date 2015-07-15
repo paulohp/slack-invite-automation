3358503766c755db9d43854aa5c2618fe76f740d
Paulo Pires <taralloqueiroiz@gmail.com>
2015-07-15 16:54:26 -0700

    better errors pages

--- routes/index.js
+++ routes/index.js
@@ -8,30 +8,26 @@ router.get('/', function(req, res) {
 });
 
 router.post('/invite', function(req, res) {
-  if (req.body.email) {
-    request.post({
-        url: 'https://'+ config.slackUrl + '/api/users.admin.invite',
-        form: {
-          email: req.body.email,
-          token: config.slacktoken,
-          set_active: true
-        }
-      }, function(err, httpResponse, body) {
-        // body looks like:
-        //   {"ok":true}
-        //       or
-        //   {"ok":false,"error":"already_invited"}
-        if (err) { return res.send('Error:' + err); }
-        body = JSON.parse(body);
-        if (body.ok) {
-          res.send('Success! Check "'+ req.body.email +'" for an invite from Slack.');
-        } else {
-          res.send('Failed! ' + body.error)
-        }
-      });
-  } else {
-    res.status(400).send('email is required.');
-  }
+  request.post({
+    url: 'https://'+ config.slackUrl + '/api/users.admin.invite',
+    form: {
+      email: req.body.email,
+      token: config.slacktoken,
+      set_active: true
+    }
+  }, function(err, httpResponse, body) {
+    // body looks like:
+    //   {"ok":true}
+    //       or
+    //   {"ok":false,"error":"already_invited"}
+    if (err) { return res.render('error',{error: err}); }
+    body = JSON.parse(body);
+    if (body.ok) {
+      res.render('success', { email: req.body.email });
+    } else {
+      res.render('error', { error: body.error })
+    }
+  });
 });
 
 module.exports = router;
